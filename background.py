6c9f28f725fd5bce2cec5fce38b33844f270866c
thinkpad-J <75423821+thinkpad-J@users.noreply.github.com>
2021-06-01 01:38:30 +0900

    Add files via upload

---
+++ background.py
@@ -0,0 +1,32 @@
+import os
+import requests
+from configparser import ConfigParser
+from datetime import datetime, timedelta
+import time
+
+serial = 111111
+
+urlstr = "http://nero666.dothome.co.kr/" + str(serial) + '.ini'
+data = requests.get(urlstr)
+data.encoding = 'utf-8'
+
+with open(str(serial) + ".ini", 'w', encoding='utf-8') as f:
+    f.write(str(data.text))
+
+
+"""
+git remote add origin https://github.com/thinkpad-J/NUSA.git
+git branch -M main
+git push -u origin main
+"""
+
+parser = ConfigParser()
+parser.read("111111.ini")
+
+stream_mode = parser.get('settings', 'StreamMode')
+stream_key = parser.get('settings', 'StreamKey')
+
+if stream_mode == 'on':
+    tm = time.localtime(time.time())
+    stime = datetime(tm.tm_year, tm.tm_mon, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec)
+    print(os.system("ffmpeg -re -i /dev/video0 -f lavfi -i anullsrc -vb 2500k -s 1280x720 -f flv rtmp://a.rtmp.youtube.com/live2/{0}".format(stream_key)))
