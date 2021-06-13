cac435ff1c9004146f0eca8aec5b1da4b6b7bcc3
thinkpad-J <75423821+thinkpad-J@users.noreply.github.com>
2021-06-13 13:58:30 +0900

    Update nusa1.py

--- nusa1.py
+++ nusa1.py
@@ -7,11 +7,16 @@ from email.mime.text import MIMEText
 import RPi.GPIO as GPIO
 from datetime import datetime, timedelta
 import time
-from background import stime
 
 global detected_time
-b_stime = stime
 global a
+global cool_time
+detected_time = 0
+a = 0
+cool_time = 0
+
+tm = time.localtime(time.time())
+stime = datetime(tm.tm_year, tm.tm_mon, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec)
 
 # serial number
 serial = 111111
@@ -24,9 +29,9 @@ GPIO.setup(shock_pin, GPIO.IN)
 GPIO.setup(motion_pin, GPIO.IN)
 
 # define sms
-# account_sid = "ACd3629a5ddec39f71bb9754cd11e3cb8c"
-# auth_token = "ded2e1b7129859a4eb65ed4d37e09656"
-# client = Client(account_sid, auth_token)
+account_sid = ""
+auth_token = ""
+client = Client(account_sid, auth_token)
 
 # ini file crawling
 urlstr = "http://nero666.dothome.co.kr/" + str(serial) + '.ini'
@@ -65,73 +70,74 @@ def send_mail_(str):
             msg = MIMEText("웹캠에 충격이 감지되었습니다.")
 
         msg['Subject'] = "NUSA에서 보낸 메일"
-        msg['To'] = "jsj2505@naver.com"
-        smtp.sendmail(email, "jsj2505@naver.com", msg.as_string())
+        msg['To'] = email
+        smtp.sendmail("jsj2505@gmail.com", email, msg.as_string())
         smtp.quit()
         print("메일을 전송했습니다.")
-"""
+
 def send_sms_(str):
     s = str
     global phone
     global a
-    arr_m = ["[nusa알림]\n", "스트리밍 시작", a, "후 모션이 감지되었습니다.\n", detected_time]
-    arr_s = ["[nusa알림]\n", "스트리밍 시작", a, "후 충격이 감지되었습니다.\n", detected_time]
+    arr_m = ["[nusa알림]\n", "스트리밍 시작", a, "후 모션이 감지되었습니다.\n"]
+    arr_s = ["[nusa알림]\n", "스트리밍 시작", a, "후 충격이 감지되었습니다.\n"]
     if phone == "01012345678" or "":
         print("phone_ number is none")
     else:
         if s == 'motion_m':
             message = client.messages.create(
-                body = "{0}{1}{2}{3}{4}".format(arr_m[0], arr_m[1], arr_m[2], arr_m[3], arr_m[4]),
-                from_ = '(812) 577-9045',
+                body = "{0}{1}{2}{3}".format(arr_m[0], arr_m[1], arr_m[2], arr_m[3]),
+                from_ = '(415) 723-6688',
                 to = phone
             )
         else:
             message = client.messages.create(
-                body = "{0}{1}{2}{3}{4}".format(arr_s[0], arr_s[1], arr_s[2], arr_s[3], arr_s[4]),
-                from_ = '(812) 577-9045',
+                body = "{0}{1}{2}{3}".format(arr_s[0], arr_s[1], arr_s[2], arr_s[3]),
+                from_ = '(415) 723-6688',
                 to = phone
             )
-    print("메세지를 보냈습니다")
-"""
+        print("메세지를 보냈습니다")
 
 """sensor shock"""
 def shock():
    global a
    global detected_time
-   global b_stime
-   while True:
-      if GPIO.input(shock_pin) == True:
+   global stime
+   global cool_time
+   
+   if GPIO.input(shock_pin) == True:
          if shock_check == "on":
-            tm2 = time.localtime(time.time())
-            detected_time = datetime(tm2.tm_year, tm2.tm_mon, tm2.tm_mday, tm2.tm_hour, tm2.tm_min, tm2.tm_sec)
-            a = detected_time - b_stime
-            send_sms_('shock_s')
-            send_mail_('shcok_s')
-            break
+            time_now = int(time.time()) 
+            if time_now - cool_time >= 0:
+                tm2 = time.localtime(time.time())
+                detected_time = datetime(tm2.tm_year, tm2.tm_mon, tm2.tm_mday, tm2.tm_hour, tm2.tm_min, tm2.tm_sec)
+                a = detected_time - stime
+                send_sms_('shock_s')
+                send_mail_('shock_s')
+                cool_time = time_now + 300 
 
 
 def motion():
-   global a
-   global detected_time
-   global b_stime
-   while True:
-      if GPIO.input(motion_pin) == True:
-         if motion_check == "on":
-            tm2 = time.localtime(time.time())
-            detected_time = datetime(tm2.tm_year, tm2.tm_mon, tm2.tm_mday,tm2.tm_hour, tm2.tm_min, tm2.tm_sec)
-            a = detected_time - b_stime
-            send_sms_('motion_m')
-            send_mail_('motion_m')
-            break
-
+    global a
+    global detected_time
+    global stime
+    global cool_time
+    
+    if GPIO.input(motion_pin) == True:
+        if motion_check == "on":
+            time_now = int(time.time())
+            if time_now - cool_time >= 0:
+                tm2 = time.localtime(time.time())
+                detected_time = datetime(tm2.tm_year, tm2.tm_mon, tm2.tm_mday, tm2.tm_hour, tm2.tm_min, tm2.tm_sec)
+                a = detected_time - stime
+                send_sms_('motion_m')
+                send_mail_('motion_m')
+                cool_time = time_now + 300 
+                
 def sm():
    while True:
       shock()
-      time.sleep(0.5)
-      print("shocked")
       motion()
-      print("motion")
-      time.sleep(300)
 
 """define"""
 
